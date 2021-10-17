b0b702049f2ea3b82671335354a4f962965551df
Ivan Lupolov <ivannaib2012@gmail.com>
2021-10-18 00:27:10 +0300

    changed interface type for CustomerReverseOrder

--- hw02-algorithms-and-generics/src/main/java/otus/java/lupolov/customer/CustomerReverseOrder.java
+++ hw02-algorithms-and-generics/src/main/java/otus/java/lupolov/customer/CustomerReverseOrder.java
@@ -1,11 +1,12 @@
 package otus.java.lupolov.customer;
 
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
-    private final LinkedList<Customer> customers = new LinkedList<>();
+    private final Deque<Customer> customers = new LinkedList<>();
 
     public void add(Customer customer) {
         customers.add(customer);
