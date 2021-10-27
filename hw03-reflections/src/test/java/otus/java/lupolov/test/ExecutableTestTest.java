5debb554f849a002baa0531066fae429186c3443
Ivan Lupolov <ivannaib2012@gmail.com>
2021-10-27 15:22:58 +0300

    homework #3 small refactoring
    * renamed test class method
    * removed unused dependencies

--- hw03-reflections/src/test/java/otus/java/lupolov/test/ExecutableTestTest.java
+++ hw03-reflections/src/test/java/otus/java/lupolov/test/ExecutableTestTest.java
@@ -6,7 +6,7 @@ import otus.java.lupolov.test.examples.ValidTestClass;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-class RunnableTestTest {
+class ExecutableTestTest {
 
     @Test
     void run_when_test_object_has_all_type_of_methods_then_all_methods_call_success() throws Exception {
