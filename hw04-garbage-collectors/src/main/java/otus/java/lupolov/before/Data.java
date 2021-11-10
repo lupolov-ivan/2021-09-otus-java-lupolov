f56dc30bd575da43c7d936d992e51cb6ae06d2cf
Ivan Lupolov <ivannaib2012@gmail.com>
2021-11-10 02:06:13 +0200

    added homework #4

---
+++ hw04-garbage-collectors/src/main/java/otus/java/lupolov/before/Data.java
@@ -0,0 +1,14 @@
+package otus.java.lupolov.before;
+
+public class Data {
+
+    private final Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
