f77f75d48bb2de9fcd4bba06f2cf1be54d839b53
Ivan Lupolov <ivannaib2012@gmail.com>
2021-11-29 21:09:06 +0200

    remove reflection from method

--- hw05-byte-codes-and-aop/src/main/java/otus/java/lupolov/proxy/LoggingInvocationHandler.java
+++ hw05-byte-codes-and-aop/src/main/java/otus/java/lupolov/proxy/LoggingInvocationHandler.java
@@ -5,30 +5,32 @@ import otus.java.lupolov.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.Optional;
 
 public class LoggingInvocationHandler implements InvocationHandler {
 
     public static final String LOG_STR_TEMPLATE = "Invoke method '%s' with param: %s%n";
 
     private final Object target;
-    private final Method[] targetMethods;
+    private final List<Method> targetMethods;
 
     public LoggingInvocationHandler(Object target) {
         this.target = target;
-        this.targetMethods = target.getClass().getMethods();
+        this.targetMethods = Arrays.stream(target.getClass().getMethods())
+                .filter(m -> m.isAnnotationPresent(Log.class))
+                .toList();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-        Method targetMethod = Arrays.stream(targetMethods)
+        Optional<Method> targetMethod = targetMethods.stream()
                 .filter(m -> m.getName().equals(method.getName()))
                 .filter(m -> Arrays.equals(m.getParameterTypes(), method.getParameterTypes()))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("No such method"));
+                .findFirst();
 
-        if (targetMethod.isAnnotationPresent(Log.class)) {
+        if (targetMethod.isPresent()) {
             System.out.printf(LOG_STR_TEMPLATE, method.getName(), Arrays.toString(args));
         }
 
