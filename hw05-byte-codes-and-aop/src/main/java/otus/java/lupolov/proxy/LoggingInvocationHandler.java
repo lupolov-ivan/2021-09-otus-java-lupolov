2234e8636d7e4db4231a6057316124ab5a00c673
Ivan Lupolov <ivannaib2012@gmail.com>
2021-11-29 18:41:55 +0200

    small optimization

--- hw05-byte-codes-and-aop/src/main/java/otus/java/lupolov/proxy/LoggingInvocationHandler.java
+++ hw05-byte-codes-and-aop/src/main/java/otus/java/lupolov/proxy/LoggingInvocationHandler.java
@@ -5,22 +5,28 @@ import otus.java.lupolov.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public class LoggingInvocationHandler implements InvocationHandler {
 
     public static final String LOG_STR_TEMPLATE = "Invoke method '%s' with param: %s%n";
 
     private final Object target;
+    private final Method[] targetMethods;
 
     public LoggingInvocationHandler(Object target) {
         this.target = target;
+        this.targetMethods = target.getClass().getMethods();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-        Class<?> targetClass = target.getClass();
-        Method targetMethod = targetClass.getMethod(method.getName(), method.getParameterTypes());
+        Method targetMethod = Arrays.stream(targetMethods)
+                .filter(m -> m.getName().equals(method.getName()))
+                .filter(m -> Arrays.equals(m.getParameterTypes(), method.getParameterTypes()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("No such method"));
 
         if (targetMethod.isAnnotationPresent(Log.class)) {
             System.out.printf(LOG_STR_TEMPLATE, method.getName(), Arrays.toString(args));
