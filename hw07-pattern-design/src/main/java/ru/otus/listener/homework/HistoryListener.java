c557237b0b99dae8594c1403b3714d883c6742a2
Ivan Lupolov <ivannaib2012@gmail.com>
2022-01-02 14:13:56 +0200

    change message container to map

--- hw07-pattern-design/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-pattern-design/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -3,23 +3,21 @@ package ru.otus.listener.homework;
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 public class HistoryListener implements Listener, HistoryReader {
 
-    private final Set<Message> messages = new HashSet<>();
+    private final Map<Long, Message> messages = new HashMap<>();
 
     @Override
     public void onUpdated(Message msg) {
-        messages.add(msg.copy());
+        messages.put(msg.getId(), msg.copy());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        return messages.stream()
-                .filter(message -> id == message.getId())
-                .findFirst();
+        return Optional.ofNullable(messages.get(id));
     }
 }
